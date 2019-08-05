c8a0647d9374a0b980d5c47aca27bba6ce481207
Luke Brobbin <lukemichaelvocalist@gmail.com>
2019-08-05 12:44:44 +0100

    chore(shop): add shop page and component

---
+++ src/pages/shop.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../components/layout/layout"
+import Shop from "../components/Shop"
+
+const ShopPage = () => {
+  return (
+    <Layout>
+      <Shop />
+    </Layout>
+  )
+}
+
+export default ShopPage
