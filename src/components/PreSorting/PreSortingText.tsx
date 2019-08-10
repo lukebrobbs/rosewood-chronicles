8ea7c92bf910e54f8a72088f6a18327a93ca452b
Luke Brobbin <lukemichaelvocalist@gmail.com>
2019-08-10 19:26:20 +0100

    chore: add signup button

--- src/components/PreSorting/PreSortingText.tsx
+++ src/components/PreSorting/PreSortingText.tsx
@@ -11,11 +11,14 @@ const PreSortingText = (props: IProps) => {
   return (
     <div className="preSorting__wrapper">
       <BannerImages />
-      <div className="preSorting__intro">
-        <h2 className="preSorting__header">House Sorting Quiz</h2>
-        <p data-testid="preSortingDescription" className="preSorting__text">
-          {props.text}
-        </p>
+      <div className="preSorting__info">
+        <div className="preSorting__intro">
+          <h2 className="preSorting__header">House Sorting Quiz</h2>
+          <p data-testid="preSortingDescription" className="preSorting__text">
+            {props.text}
+          </p>
+        </div>
+        <button className="preSorting__signUp__button">SIGN UP</button>
       </div>
     </div>
   )
