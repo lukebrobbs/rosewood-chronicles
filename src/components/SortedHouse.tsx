78de0022814c260b5089cea27b5c2e62e66ab196
Luke <lbrobbin@jaguarlandrover.com>
2019-09-02 14:42:11 +0100

    chore: move roses for mobile

--- src/components/SortedHouse.tsx
+++ src/components/SortedHouse.tsx
@@ -1,5 +1,5 @@
-import React, { FunctionComponent } from "react"
 import Img from "gatsby-image"
+import React, { FunctionComponent } from "react"
 import { ISortedHouseProps } from "../types"
 
 export const SortedHouse: FunctionComponent<ISortedHouseProps> = props => {
