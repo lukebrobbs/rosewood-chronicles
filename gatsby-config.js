40000781a1488962416e2e25cf317d35897ae207
Luke <lbrobbin@jaguarlandrover.com>
2019-09-04 11:53:11 +0100

    chore: add new favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,13 +32,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `the-rosewood-chronicles`,
+        short_name: `Rosewood`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/Favicon.png`, // This path is relative to the root of the site.
       },
     },
   ],
