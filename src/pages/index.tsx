d9d9f478bfa1266ccecba62a1b2c6d754149d6b2
Luke Brobbin <lukemichaelvocalist@gmail.com>
2020-01-13 18:44:21 +0000

    chore: add feature flag for version 2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,31 @@
 import { graphql } from "gatsby"
 import React from "react"
-// import Home from "../components/Home/Home"
-// import Nav from "../components/Nav"
+import Home from "../components/Home/Home"
+import Nav from "../components/Nav"
 import { NonHomePages } from "../components/NonHomePages"
-// import { Element } from "../components/Scroll"
-// import SEO from "../components/seo"
-// import RosesTopLeft from "../images/Roses_TL.svg"
-// import RosesTopRight from "../images/Roses_TR.svg"
-// import RosesBottomLeft from "../images/Roses_BL.svg"
-// import RosesBottomRight from "../images/Roses_BR.svg"
+import { Element } from "../components/Scroll"
+import SEO from "../components/seo"
+import RosesTopLeft from "../images/Roses_TL.svg"
+import RosesTopRight from "../images/Roses_TR.svg"
+import RosesBottomLeft from "../images/Roses_BL.svg"
+import RosesBottomRight from "../images/Roses_BR.svg"
 
 const IndexPage = ({ data }) => {
   return (
     <>
-      {/* <SEO title="Home" /> */}
-      {/* <img src={RosesTopLeft} className="home__RosesTopLeft" />
-      <img src={RosesTopRight} className="home__RosesTopRight" />
-      <img src={RosesBottomLeft} className="home__RosesBottomLeft" />
-      <img src={RosesBottomRight} className="home__RosesBottomRight" />
-      <Element name="title-page">
-        <Home />
-      </Element> */}
-      {/* <Nav /> */}
+      {process.env.GATSBY_VERSION_TWO && (
+        <>
+          <SEO title="Home" />
+          <img src={RosesTopLeft} className="home__RosesTopLeft" />
+          <img src={RosesTopRight} className="home__RosesTopRight" />
+          <img src={RosesBottomLeft} className="home__RosesBottomLeft" />
+          <img src={RosesBottomRight} className="home__RosesBottomRight" />
+          <Element name="title-page">
+            <Home />
+          </Element>
+          <Nav />
+        </>
+      )}
       <NonHomePages data={data} />
     </>
   )
