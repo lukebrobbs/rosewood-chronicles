eafccd83c55437a7cd2bcd3f8a5f88b95f811a5f
Luke Brobbin <lukemichaelvocalist@gmail.com>
2020-01-16 19:41:01 +0000

    chore: add circleCI config to cypress

--- cypress/integration/home.spec.ts
+++ cypress/integration/home.spec.ts
@@ -1,6 +1,6 @@
 describe("Home page", () => {
   it("Should link to sorting quiz", () => {
-    cy.visit("http://localhost:8000")
+    cy.visit("/")
     cy.get("[data-testid=home_sortingQuiz_link]").click()
     cy.contains("house sorting quiz")
   })
