bf6c79600f60ddc4cf4170b370ebe97f596ba73b
Luke Brobbin <lukemichaelvocalist@gmail.com>
2020-03-03 13:59:14 +0000

    chore: add book image to yearbook page

--- src/pages/yearbook.tsx
+++ src/pages/yearbook.tsx
@@ -1,14 +1,15 @@
 import React, { FunctionComponent } from "react"
 import SEO from "../components/seo"
-import { StudentsRoutes } from "../components/StudentsRoutes"
 import { graphql } from "gatsby"
 import { YearbookProps } from "../types"
 
-const Yearbook: FunctionComponent<YearbookProps> = props => {
+const Yearbook: FunctionComponent<YearbookProps> = () => {
   return (
     <>
       <SEO title="Yearbook" />
-      <StudentsRoutes data={props.data} />
+      <div className="yearbook__wrapper">
+        <div className="yearbook__image__wrapper"></div>
+      </div>
     </>
   )
 }
