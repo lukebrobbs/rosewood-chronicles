d7b2f98b9181e95a835cfabd4f4206753ca133cc
Luke Brobbin <lbrobbin@jaguarlandrover.com>
2020-03-22 14:06:56 +0000

    chore: add studetn descriptiosn

--- src/components/YearbookStudent.tsx
+++ src/components/YearbookStudent.tsx
@@ -29,7 +29,7 @@ export const YearbookStudent: FunctionComponent<YearbookStudentProps> = props =>
       </Link>
 
       <div className="yearbook__wrapper">
-        <div className="yearbook__image__wrapper">
+        <div className="individual__yearbook__wrapper">
           <div className="yearbook__pageOne">
             <h1 className="yearbook__header">
               {props.data.contentfulStudentDescription.name}
@@ -46,6 +46,33 @@ export const YearbookStudent: FunctionComponent<YearbookStudentProps> = props =>
                 }
               />
             </div>
+          </div>
+          <div className="yearbook__pageTwo">
+            <h2>Birthday</h2>
+            <p>{props.data.contentfulStudentDescription.birthday}</p>
+            <h2>Favorite Things</h2>
+            <p>
+              {
+                props.data.contentfulStudentDescription.favouriteThings
+                  .favouriteThings
+              }
+            </p>
+            <h2>Least Favorite Things</h2>
+            <p>
+              {
+                props.data.contentfulStudentDescription.leastFavouriteThings
+                  .leastFavouriteThings
+              }
+            </p>
+            <h2>In Their Bag</h2>
+            <p>
+              {props.data.contentfulStudentDescription.inTheirBag.inTheirBag}
+            </p>
+            <h2>Quote</h2>
+            <p>{props.data.contentfulStudentDescription.quote.quote}</p>
+            <Link to={`/${props.pageContext.house}`}>
+              Learn more about {props.pageContext.house} house
+            </Link>
           </div>
         </div>
       </div>
