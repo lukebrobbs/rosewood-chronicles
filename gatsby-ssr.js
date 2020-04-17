4eb414537588726dfc054e5db7ee76aacd202bf6
Luke Brobbin <lukemichaelvocalist@gmail.com>
2020-04-17 13:05:02 +0100

    chore: add SSR support

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,35 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/ssr-apis/
- */
+const React = require("react")
+const { Helmet } = require("react-helmet")
+const { Layout } = require("./src/components/layout/layout")
+const { Navigation } = require("./src/components/Nav")
+const Modal = require("react-modal")
+require("./src/components/layout/layout.scss")
 
-// You can delete this file if you're not using it
+
+
+exports.wrapRootElement = ({ element }) => {
+  Modal.setAppElement("#___gatsby")
+
+  return (
+    <>
+      <Helmet>
+        <link
+          rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+          crossOrigin="anonymous"
+        />
+      </Helmet>
+      {element}
+    </>
+  )
+}
+
+exports.wrapPageElement = ({ element, props }) => {
+  return (
+    <Layout page={props.path}>
+      <Navigation />
+      {element}
+    </Layout>
+  )
+}
